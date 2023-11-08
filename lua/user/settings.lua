6de674b2adbab62038ade749b6724f0f026c9a6a
Scott Lampert <scott@lampert.org>
2023-11-07 21:47:59 -0500

    Add neovide settings

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -21,3 +21,15 @@ vim.opt.cindent = true
 vim.opt.cinoptions = 'g0:0t0c2C1(0f0l1'
 vim.opt.signcolumn = 'yes'
 vim.opt.termguicolors = true
+
+-- Neovide Specific
+if vim.g.neovide then
+  -- vim.o.guifont = "GoMono Nerd Font:h12:#e-subpixelantialias"
+  vim.o.guifont = "FantasqueSansM Nerd Font:h13:#e-subpixelantialias"
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_theme = 'auto'
+
+  -- vim.g.neovide_cursor_vfx_mode = "railgun"
+  -- vim.g.neovide_cursor_vfx_mode = "torpedo"
+  vim.g.neovide_cursor_vfx_mode = "pixiedust"
+end
