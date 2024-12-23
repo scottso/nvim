eed08ee1bb88ba910952269d32096e76311abfad
Scott Lampert <scott@lampert.org>
2024-12-23 12:42:58 -0500

    More blink-cmp configuration

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,10 +1,12 @@
 return {
   "saghen/blink.cmp",
   -- optional: provides snippets for the snippet source
-  -- dependencies = 'rafamadriz/friendly-snippets',
+  dependencies = "rafamadriz/friendly-snippets",
 
+  event = "InsertEnter",
+  --
   -- use a release tag to download pre-built binaries
-  -- version = 'v0.*',
+  version = "v0.*",
   -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
   -- build = 'cargo build --release',
   -- If you use nix, you can build from source using latest nightly rust with:
@@ -13,35 +15,84 @@ return {
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
-    -- 'default' for mappings similar to built-in completion
-    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-    -- see the "default configuration" section below for full documentation on how to define
-    -- your own keymap.
-    keymap = { preset = "default" },
+    -- Disable for some filetypes
+    enabled = function()
+      return not vim.tbl_contains({ "lua", "markdown" }, vim.bo.filetype)
+        and vim.bo.buftype ~= "prompt"
+        and vim.b.completion ~= false
+    end,
 
-    appearance = {
-      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-      -- Useful for when your theme doesn't support blink.cmp
-      -- will be removed in a future release
-      use_nvim_cmp_as_default = true,
-      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = "mono",
+    completion = {
+      -- 'prefix' will fuzzy match on the text before the cursor
+      -- 'full' will fuzzy match on the text before *and* after the cursor
+      -- example: 'foo_|_bar' will match 'foo_' for 'prefix' and 'foo__bar' for 'full'
+      keyword = { range = "full" },
+
+      -- Disable auto brackets
+      -- NOTE: some LSPs may add auto brackets themselves anyway
+      accept = { auto_brackets = { enabled = false } },
+
+      -- Insert completion item on selection, don't select by default
+      list = { selection = "auto_insert" },
+
+      menu = {
+        -- Don't automatically show the completion menu
+        auto_show = false,
+
+        draw = {
+          components = {
+            kind_icon = {
+              ellipsis = false,
+              text = function(ctx)
+                local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+                return kind_icon
+              end,
+              -- Optionally, you may also use the highlights from mini.icons
+              highlight = function(ctx)
+                local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                return hl
+              end,
+            },
+          },
+        },
+      },
+
+      -- Show documentation when selecting a completion item
+      documentation = { auto_show = true, auto_show_delay_ms = 500 },
+
+      -- Display a preview of the selected item on the current line
+      ghost_text = { enabled = true },
     },
 
-    -- default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
-      -- optionally disable cmdline completions
+      -- Remove 'buffer' if you don't want text completions, by default it's only enabled when LSP returns no items
+      -- default = { "lsp", "path", "snippets", "buffer" },
+
+      -- Disable cmdline completions
       cmdline = {},
+
+      -- Dynamically picking providers by treesitter node/filetype
+      default = function()
+        local success, node = pcall(vim.treesitter.get_node)
+        if vim.bo.filetype == "lua" then
+          return { "lsp", "path" }
+        elseif success and node and vim.tbl_contains({ "comment", "line_comment", "block_comment" }, node:type()) then
+          return { "buffer" }
+        else
+          return { "lsp", "path", "snippets", "buffer" }
+        end
+      end,
     },
 
-    -- experimental signature help support
-    -- signature = { enabled = true }
+    -- Experimental signature help support
+    signature = { enabled = true },
   },
+
   -- allows extending the providers array elsewhere in your config
   -- without having to redefine it
   opts_extend = { "sources.default" },
+
+  keymap = {
+    preset = "default",
+  },
 }
