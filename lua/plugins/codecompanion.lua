66610ef2644fc517a6a0ebf8e8c0eed1b9789ea7
Scott Lampert <scott@lampert.org>
2025-03-16 15:38:23 -0400

    Switch to codecompanion

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,21 +1,118 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+--if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+---@diagnostic disable-next-line: unused-local
+local function generate_slash_commands()
+  local commands = {}
+  for _, command in ipairs { "buffer", "file", "help", "symbols" } do
+    commands[command] = {
+      opts = {
+        provider = "fzf_lua",
+      },
+    }
+  end
+  return commands
+end
 
 ---@type LazySpec
 return {
   {
     "olimorris/codecompanion.nvim",
-    config = true,
     dependencies = {
-      { "nvim-lua/plenary.nvim", branch = "master" },
+      "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
+      { "MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } },
     },
     opts = {
       strategies = {
         chat = {
           adapter = "anthropic",
+          roles = {
+            llm = "CodeCompanion",
+            user = "Me",
+          },
+          slash_commands = generate_slash_commands(),
+          keymaps = {
+            close = {
+              modes = {
+                n = "q",
+              },
+              index = 3,
+              callback = "keymaps.close",
+              description = "Close Chat",
+            },
+            stop = {
+              modes = {
+                n = "<C-c",
+              },
+              index = 4,
+              callback = "keymaps.stop",
+              description = "Stop Request",
+            },
+          },
         },
-        inline = {
-          adapter = "anthropic",
+      },
+      inline = {
+        adapter = "anthropic",
+      },
+    },
+    keys = {
+      {
+        "<leader>ac",
+        "<cmd>CodeCompanionActions<cr>",
+        mode = { "n", "v" },
+        noremap = true,
+        silent = true,
+        desc = "CodeCompanion actions",
+      },
+      {
+        "<leader>aa",
+        "<cmd>CodeCompanionChat Toggle<cr>",
+        mode = { "n", "v" },
+        noremap = true,
+        silent = true,
+        desc = "CodeCompanion chat",
+      },
+      {
+        "<leader>ad",
+        "<cmd>CodeCompanionChat Add<cr>",
+        mode = "v",
+        noremap = true,
+        silent = true,
+        desc = "CodeCompanion add to chat",
+      },
+    },
+  },
+  {
+    "saghen/blink.compat",
+    -- use the latest release, via version = '*', if you also use the latest release for blink.cmp
+    version = "*",
+    -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+    lazy = true,
+    -- make sure to set opts so that lazy.nvim calls blink.compat's setup
+    opts = {},
+  },
+  {
+    "saghen/blink.cmp",
+    dependencies = { "olimorris/codecompanion.nvim", "saghen/blink.compat" },
+    event = "InsertEnter",
+    opts = {
+      enabled = function() return vim.bo.buftype ~= "prompt" and vim.b.completion ~= false end,
+      completion = {
+        accept = {
+          auto_brackets = {
+            kind_resolution = {
+              blocked_filetypes = { "typescriptreact", "javascriptreact", "vue", "codecompanion" },
+            },
+          },
+        },
+      },
+      sources = {
+        providers = {
+          codecompanion = {
+            name = "CodeCompanion",
+            module = "codecompanion.providers.completion.blink",
+            enabled = true,
+          },
         },
       },
     },
