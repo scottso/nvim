0d37f12aa1fdb33ab95ea2c04a9ac39e8a789646
Scott Lampert <scott@lampert.org>
2025-03-31 21:53:57 -0400

    Configure virtual text and lines

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -7,97 +7,128 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocore",
-  ---@type AstroCoreOpts
-  opts = {
-    -- Configure core features of AstroNvim
-    features = {
-      large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
-      cmp = true, -- enable completion at start
-      diagnostics = { virtual_text = true, virtual_lines = false }, -- diagnostic settings on startup
-      highlighturl = true, -- highlight URLs at start
-      notifications = true, -- enable notifications at start
-    },
-    -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-    diagnostics = {
-      virtual_text = true,
-      underline = true,
-    },
-    -- vim options can be configured here
-    options = {
-      opt = { -- vim.opt.<key>
-        foldenable = false,
-        foldcolumn = "0",
-        relativenumber = true, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
-        signcolumn = "yes", -- sets vim.opt.signcolumn to auto
-        wrap = false, -- sets vim.opt.wrap
-        mouse = "",
-        ignorecase = true,
-        smartcase = true,
-        hlsearch = false,
-        breakindent = true,
-        tabstop = 2,
-        shiftwidth = 2,
-        expandtab = false,
-        undofile = true,
-        undolevels = 1500, -- how many times the user can undo
-        undoreload = 10000,
-        autoindent = true,
-        cindent = true,
-        cinoptions = "g0:0t0c2C1(0f0l1",
-        termguicolors = true,
-      },
-      g = { -- vim.g.<key>
-        -- configure global vim variables (vim.g)
-        -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
-        -- This can be found in the `lua/lazy_setup.lua` file
-      },
-    },
-    -- Mappings can be configured through AstroCore as well.
-    -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
-    mappings = {
-      -- first key is the mode
-      n = {
-        -- second key is the lefthand side of the map
+	"AstroNvim/astrocore",
+	---@type AstroCoreOpts
+	opts = {
+		-- Configure core features of AstroNvim
+		features = {
+			large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+			autopairs = true, -- enable autopairs at start
+			cmp = true, -- enable completion at start
+			diagnostics = { virtual_text = true, virtual_lines = true }, -- diagnostic settings on startup
+			highlighturl = true, -- highlight URLs at start
+			notifications = true, -- enable notifications at start
+		},
+		-- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
+		diagnostics = {
+			virtual_text = {
+				enabled = true,
+				severity = {
+					max = vim.diagnostic.severity.WARN,
+				},
+			},
+			virtual_lines = {
+				enabled = true,
+				severity = {
+					min = vim.diagnostic.severity.ERROR,
+				},
+			},
+			underline = true,
+		},
+		-- vim options can be configured here
+		options = {
+			opt = { -- vim.opt.<key>
+				foldenable = false,
+				foldcolumn = "0",
+				relativenumber = true, -- sets vim.opt.relativenumber
+				number = true, -- sets vim.opt.number
+				spell = false, -- sets vim.opt.spell
+				signcolumn = "yes", -- sets vim.opt.signcolumn to auto
+				wrap = false, -- sets vim.opt.wrap
+				mouse = "",
+				ignorecase = true,
+				smartcase = true,
+				hlsearch = false,
+				breakindent = true,
+				tabstop = 2,
+				shiftwidth = 2,
+				expandtab = false,
+				undofile = true,
+				undolevels = 1500, -- how many times the user can undo
+				undoreload = 10000,
+				autoindent = true,
+				cindent = true,
+				cinoptions = "g0:0t0c2C1(0f0l1",
+				termguicolors = true,
+			},
+			g = { -- vim.g.<key>
+				-- configure global vim variables (vim.g)
+				-- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
+				-- This can be found in the `lua/lazy_setup.lua` file
+			},
+		},
+		-- Mappings can be configured through AstroCore as well.
+		-- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
+		mappings = {
+			-- first key is the mode
+			n = {
+				-- second key is the lefthand side of the map
 
-        -- navigate buffer tabs
-        ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-        ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+				-- navigate buffer tabs
+				["]b"] = {
+					function()
+						require("astrocore.buffer").nav(vim.v.count1)
+					end,
+					desc = "Next buffer",
+				},
+				["[b"] = {
+					function()
+						require("astrocore.buffer").nav(-vim.v.count1)
+					end,
+					desc = "Previous buffer",
+				},
 
-        -- mappings seen under group name "Buffer"
-        ["<Leader>bd"] = {
-          function()
-            require("astroui.status.heirline").buffer_picker(
-              function(bufnr) require("astrocore.buffer").close(bufnr) end
-            )
-          end,
-          desc = "Close buffer from tabline",
-        },
+				-- mappings seen under group name "Buffer"
+				["<Leader>bd"] = {
+					function()
+						require("astroui.status.heirline").buffer_picker(function(bufnr)
+							require("astrocore.buffer").close(bufnr)
+						end)
+					end,
+					desc = "Close buffer from tabline",
+				},
 
-        -- navigate buffer tabs with `H` and `L`
-        L = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-        H = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+				-- navigate buffer tabs with `H` and `L`
+				L = {
+					function()
+						require("astrocore.buffer").nav(vim.v.count1)
+					end,
+					desc = "Next buffer",
+				},
+				H = {
+					function()
+						require("astrocore.buffer").nav(-vim.v.count1)
+					end,
+					desc = "Previous buffer",
+				},
 
-        -- tables with just a `desc` key will be registered with which-key if it's installed
-        -- this is useful for naming menus
-        ["<Leader>b"] = { desc = "Buffers" },
-        ["<Leader>w"] = { desc = "Window" },
+				-- tables with just a `desc` key will be registered with which-key if it's installed
+				-- this is useful for naming menus
+				["<Leader>b"] = { desc = "Buffers" },
+				["<Leader>w"] = { desc = "Window" },
 
-        -- mappings seen under group name "Buffer"
-        ["<Leader>bD"] = {
-          function()
-            require("astroui.status.heirline").buffer_picker(
-              function(bufnr) require("astrocore.buffer").close(bufnr) end
-            )
-          end,
-          desc = "Pick to close",
-        },
-        -- setting a mapping to false will disable it
-        -- ["<C-S>"] = false,
-      },
-    },
-  },
+				-- mappings seen under group name "Buffer"
+				["<Leader>bD"] = {
+					function()
+						require("astroui.status.heirline").buffer_picker(function(bufnr)
+							require("astrocore.buffer").close(bufnr)
+						end)
+					end,
+					desc = "Pick to close",
+				},
+				-- setting a mapping to false will disable it
+				-- ["<C-S>"] = false,
+			},
+		},
+	},
 }
